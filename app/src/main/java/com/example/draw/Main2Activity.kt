8e693b07b984e07a51db9f984b0fa9824869ea9d
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-07 16:19:10 +0800

    MainActivity layout 2  ImageView move

--- app/src/main/java/com/example/draw/Main2Activity.kt
+++ app/src/main/java/com/example/draw/Main2Activity.kt
@@ -3,6 +3,7 @@ package com.example.draw
 import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
