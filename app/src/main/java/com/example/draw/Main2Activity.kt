a5874439c53a40187ba5cefc4d8779368d269421
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-07 16:13:48 +0800

    MainActivity layout 2 add a ImageView

--- app/src/main/java/com/example/draw/Main2Activity.kt
+++ app/src/main/java/com/example/draw/Main2Activity.kt
@@ -1,10 +1,10 @@
 package com.example.draw
 
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Main2Activity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
