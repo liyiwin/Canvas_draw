f5ead29780c089d7278f32f1565ef21d4d64af86
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-07 16:41:24 +0800

    finished

--- app/src/main/java/com/example/draw/Main2Activity.kt
+++ app/src/main/java/com/example/draw/Main2Activity.kt
@@ -9,6 +9,12 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val extra = intent.getStringExtra("shap")
+
+        setView(extra!!)
+
+
     }
 
 
